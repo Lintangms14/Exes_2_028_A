de68145d13877373c5338cc54f3152d643b3005b
Lintangms14 <merayusukmalintang@gmail.com>
2023-05-11 10:38:45 +0700

    Exricise2

--- exircise2/exircise2.cpp
+++ exircise2/exircise2.cpp
@@ -1,20 +1,32 @@
-// exircise2.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+int main() {
+	int arr[] = { 1, 3, 5, 11, 13, 15};
+	int n = sizeof(arr) / sizeof(arr[0]);
+
+	int x;
+	cout << "Masukkan Element Yang Ingin Di Cari: ";
+	cin >> x;
+
+	int lowerbound = 0;
+	int upperbound = n - 1;
+
+	while (lowerbound <= upperbound) {
+		int mid = (lowerbound + upperbound) / 2;
+		if (arr[mid] == x) {
+			cout << "Element Di Temukan Di Indeks Ke" << mid << endl;
+			break;
+		}
+		else if (x < arr[mid]) {
+			upperbound = mid - 1;
+		}
+		else {
+			lowerbound = mid + 1;
+		}
+	}
+	if (lowerbound > upperbound) {
+		cout << "Element Tidak Di Temukan" << endl;
+	}
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
